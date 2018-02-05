fb3f166171fa4469f6b3900eb74632c38c611e81
Rajdeep Nanua <rajdeep.nanua@mail.utoronto.ca>
2018-02-04 20:14:27 -0500

    Added a user input check for git force commands

--- js/terminal.js
+++ js/terminal.js
@@ -1,33 +1,45 @@
-var os = require('os');
-var pty = require('node-pty');
-var Terminal = require('xterm').Terminal;
-var fit = require('xterm/lib/addons/fit/fit').fit;
+const os = require('os');
+const pty = require('node-pty');
+const Terminal = require('xterm').Terminal;
+const fit = require('xterm/lib/addons/fit/fit');
+const KeyCode = require('keycode-js');
 
-var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+// Initialize a shell process
+let shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
 
-var ptyProcess = pty.spawn(shell, [], {
-  name: 'xterm-color',
-  cols: 80,
-  rows: 30,
-  cwd: process.cwd(),
-  env: process.env
+let ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cwd: process.cwd(),
+    env: process.env
 });
 
+// Initialize the front end for the shell process
 let xterm = new Terminal({
-  cursorBlink: true,
-  cols: 120,
-  rows: 80
+    cursorBlink: true,
 });
 
 xterm.open(document.getElementById('terminal'));
-fit(xterm);
+fit.fit(xterm);
 
 xterm.on('data', (data) => {
-  ptyProcess.write(data);
-  console.log("xterm", data);
+    // Check user command after pressing enter
+    if (data.charCodeAt(0) === KeyCode.KEY_RETURN) {
+        let lineIdx = xterm.buffer.y + xterm.buffer.ybase;
+        let currLine = xterm.buffer.lines._array[lineIdx];
+
+        currLine = currLine.map((arrElement) => {
+            return arrElement[1];
+        }).join("");
+
+        if (inputChecker(currLine) == true) {
+            ptyProcess.write(data);
+        }
+    } else { // If user didn't press enter, pass the input to the shell
+        ptyProcess.write(data);
+    }
 });
 
+// Send the shell output to the front end
 ptyProcess.on('data', (data) => {
-  xterm.write(data);
-  console.log("ptyprocess", data);
+    xterm.write(data);
 });
